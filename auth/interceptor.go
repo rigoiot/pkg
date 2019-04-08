3689995f5d04a100618e5b5186a5f2f8a72bc059
Tim <mythfish@gmail.com>
2019-04-08 17:29:24 +0800

    Fix interceptor

--- auth/interceptor.go
+++ auth/interceptor.go
@@ -9,7 +9,7 @@ import (
 )
 
 // UnaryServerInterceptor returns a new unary server interceptor that inject grpc client.
-func UnaryServerInterceptor(dmConn *grpc.ClientConn) grpc.UnaryServerInterceptor {
+func UnaryServerInterceptor() grpc.UnaryServerInterceptor {
 	return func(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (interface{}, error) {
 		// put account id in metadata
 		accountID, err := GetAccountID(ctx, nil)
