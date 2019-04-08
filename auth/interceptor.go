1e83c9c5ce4de9e78911f3a6e34f5491673bbf15
Tim <mythfish@gmail.com>
2019-04-08 17:27:13 +0800

    Add grpc Interceptor for auth

---
+++ auth/interceptor.go
@@ -0,0 +1,22 @@
+package auth
+
+import (
+	"context"
+
+	"google.golang.org/grpc/metadata"
+
+	"google.golang.org/grpc"
+)
+
+// UnaryServerInterceptor returns a new unary server interceptor that inject grpc client.
+func UnaryServerInterceptor(dmConn *grpc.ClientConn) grpc.UnaryServerInterceptor {
+	return func(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (interface{}, error) {
+		// put account id in metadata
+		accountID, err := GetAccountID(ctx, nil)
+		if err == nil {
+			md := metadata.Pairs("AccountID", accountID.String())
+			ctx = metadata.NewOutgoingContext(ctx, md)
+		}
+		return handler(ctx, req)
+	}
+}
