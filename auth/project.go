77bad7937bfe7fd0e1cb9b60a2276f46cd4c5ccb
linkh <linkh@rigoiot.com>
2026-06-11 10:18:16 +0800

    feat: add projectID

---
+++ auth/project.go
@@ -0,0 +1,29 @@
+package auth
+
+import (
+	"context"
+	"errors"
+
+	"github.com/grpc-ecosystem/go-grpc-middleware/util/metautils"
+	uuid "github.com/satori/go.uuid"
+)
+
+var (
+	errMissingProjectID = errors.New("unable to get project id from context")
+
+	// ProjectIDKey is the metadata key used to carry project identity.
+	ProjectIDKey = "ProjectID"
+)
+
+// GetProjectID gets the projectID from a context.
+func GetProjectID(ctx context.Context, _ interface{}) (uuid.UUID, error) {
+	val := metautils.ExtractIncoming(ctx).Get(ProjectIDKey)
+	if val == "" {
+		return uuid.Nil, errMissingProjectID
+	}
+	id, err := uuid.FromString(val)
+	if err != nil {
+		return uuid.Nil, errMissingProjectID
+	}
+	return id, nil
+}
